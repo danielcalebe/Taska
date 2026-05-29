278735a28ed72df354f8239dada44452b0ed767a
danielcalebe <danielcalebe719@gmail.com>
2026-05-29 08:38:59 -0300

    feat: Adicionando dependencias, assets, imagens, cores e tema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21"
 }
 
 android {
@@ -37,6 +38,7 @@ android {
 }
 
 dependencies {
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/taska/taska/ui/theme/Theme.kt
+++ app/src/main/java/com/taska/taska/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -18,9 +19,17 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-  primary = Purple40,
-  secondary = PurpleGrey40,
-  tertiary = Pink40
+
+  background = Color(0xFFF7F7F7),
+  surface = Color(0xFFEDEDED),
+  primary = Color(0xFFAED6F1),
+  secondary = Color(0xFFA2DED0),
+  onSurface = Color(0xFF333333),
+  onPrimary = Color(0xFF333333),
+  onSecondary = Color(0xFF333333),
+  error = Color(0xFFF44336),
+  onError = Color(0xFFF44336),
+
 
   /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +46,7 @@ private val LightColorScheme = lightColorScheme(
 fun TaskaTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
-  dynamicColor: Boolean = true,
+  dynamicColor: Boolean = false,
   content: @Composable () -> Unit
 ) {
   val colorScheme = when {
@@ -51,7 +60,7 @@ fun TaskaTheme(
   }
 
   MaterialTheme(
-    colorScheme = colorScheme,
+    colorScheme = LightColorScheme,
     typography = Typography,
     content = content
   )
